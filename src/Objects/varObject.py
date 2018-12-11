af51a564f598a18eeaa31baf882bf5e195915783
thenerdsuperuser <parthkrishna1611@gmail.com>
2018-12-11 15:05:23 +0530

    Creating objects to execute the code

---
+++ src/Objects/varObject.py
@@ -0,0 +1,12 @@
+
+
+class VariableObject(object):
+
+    def __init__(self):
+        # This will hold the exec string for var declaration
+        self.exec_string = ""
+
+    def transpile(self, name, operator, value):
+        # return the string to be executed
+        self.exec_string += name + " " + operator + " " + value
+        return self.exec_string
