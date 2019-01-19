4dd1ce1cd340a7c088d4f377c4a370a8a9362a76
Parth Krishna <parthkrishna1611@gmail.com>
2019-01-19 11:58:33 +0530

    Added comments and keywords to the lexer.

--- src/lexer.py
+++ src/lexer.py
@@ -1,11 +1,44 @@
-#!/usr/bin/python3
+class Lexer:
 
-# read file
-with open("hello.poo", "r") as code:
-    # print lines
-    for line in code.readlines():
-        print(line)
-        
-        # print words
-        for word in line.split():
-            print(word)
+    def __init__(self, data):
+        self.data = data
+        self.tokens = []
+        self.comm = []
+        self.keywords = [
+                'vomit'
+                ]
+
+    def tokenizer(self):
+
+        for loc in self.data:
+            tmp = []
+            com = []
+            tid = ''
+
+            for l in loc:
+                if l == '"' and tid == '':
+                    tid = 'char'
+                    tmp = []
+
+                elif l == '"' and tid == 'char':
+                    self.tokens.append({'id':tid, 'value': ''.join(tmp)})
+                    tid = ''
+                    tmp = []
+
+                elif ''.join(tmp) in self.keywords:
+                    self.tokens.append({'id': 'keyword', 'value': ''.join(tmp)})
+                    tmp = []
+
+                elif l == ' ' and tid != 'char':
+                    continue
+                
+                elif l == '/*' and tid == '':
+                    tid = 'comment'
+                    tmp = []
+
+                elif l == '*/' and tid == 'comment':
+                    self.comm.append({'id': tid, 'value': ''.join(com)})
+                
+                else:
+                    tmp.append(l)
+                    com.append(l)
---
+++ src/poodle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from lexer import Lexer
+from pprint import pprint
+
+def main():
+    filename = 'hello.poo'
+    file = open(filename, 'r')
+    lexer = Lexer(file)
+
+    lexer.tokenizer()
+    #print("Tokens:")
+    pprint(lexer.tokens)
+    pprint(lexer.comm)
+
+
+if __name__ == "__main__":
+    main()
